23a7ffa05b86c6f3a6a259c8cfcfa32a02aa46b8
Caio Campos <20524962+caiocampos@users.noreply.github.com>
2026-06-30 16:35:12 -0300

    change test configuration

--- src/test/kotlin/br/campos/poi/POIIncApplicationTests.kt
+++ src/test/kotlin/br/campos/poi/POIIncApplicationTests.kt
@@ -2,7 +2,7 @@ package br.campos.poi
 
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.junit.jupiter.SpringExtension
 
